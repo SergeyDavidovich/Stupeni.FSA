f48e0607d085b81c8baf915b9f9203cfa907cd6f
Bekzod Allaev <allaev_@outlook.com>
2024-03-15 17:34:21 +0200

    Add price for flight

--- src/Stupeni.FSA.Domain/Entities/Flight.cs
+++ src/Stupeni.FSA.Domain/Entities/Flight.cs
@@ -53,5 +53,7 @@ namespace Stupeni.FSA.Entities
         /// Навигационное свойство
         /// </summary>
         public Booking Booking { get; set; }
+
+        public double Price { get; set; }
     }
 }
