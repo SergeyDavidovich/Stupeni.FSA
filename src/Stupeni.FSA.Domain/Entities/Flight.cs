679b7990ba9e4b5a4064f99e56f9a187415df72c
Bekzod Allaev <allaev_@outlook.com>
2024-03-15 17:08:57 +0200

    Change type of DaysOfOperation from List<int> to List<DayOfWeek>

--- src/Stupeni.FSA.Domain/Entities/Flight.cs
+++ src/Stupeni.FSA.Domain/Entities/Flight.cs
@@ -20,9 +20,9 @@ namespace Stupeni.FSA.Entities
         public string FlightNumber { get; set; }
 
         /// <summary>
-        /// Дни полета. Может быть в диапозоне от 1 до 7, где 1 - это понедельник, а 7 - это воскресенье 
+        /// Дни полета
         /// </summary>
-        public List<int> DaysOfOperation { get; set; }
+        public List<DayOfWeek> DaysOfOperation { get; set; }
 
         /// <summary>
         /// Город вылета
