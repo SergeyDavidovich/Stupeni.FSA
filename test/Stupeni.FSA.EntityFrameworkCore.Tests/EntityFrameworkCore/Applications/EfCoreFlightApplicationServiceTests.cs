090a46e78691e85ab701566ec122d1aef77c9c03
Bekzod Allaev <allaev_@outlook.com>
2024-03-16 16:38:04 +0200

    Implement abstract test class - FlightsApplicationLayerTest so it will be visible in Test Explorer

---
+++ test/Stupeni.FSA.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/EfCoreFlightApplicationServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using Stupeni.FSA.Flights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stupeni.FSA.EntityFrameworkCore.Applications
+{
+    public class EfCoreFlightApplicationServiceTests : FlightsApplicationLayerTest<FSAEntityFrameworkCoreTestModule>
+    {
+    }
+}
