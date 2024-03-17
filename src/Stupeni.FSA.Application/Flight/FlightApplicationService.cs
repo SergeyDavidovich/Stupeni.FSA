22033f391b373274fe18d598e0cc4177484f7c4b
BMinin <boris.minin@outlook.com>
2024-03-17 17:01:26 +0500

    Update FlightApplicationService

--- src/Stupeni.FSA.Application/Flight/FlightApplicationService.cs
+++ src/Stupeni.FSA.Application/Flight/FlightApplicationService.cs
@@ -1,4 +1,5 @@
-﻿using Stupeni.FSA.Flights;
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Stupeni.FSA.Flights;
 using Stupeni.FSA.Flights.DataSource;
 using Stupeni.FSA.Flights.Dto;
 using System;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
+using Volo.Abp.Caching;
 
 namespace Stupeni.FSA.Flight
 {
@@ -14,16 +16,26 @@ namespace Stupeni.FSA.Flight
     {
         private readonly ICISFlightsSource _cisFlightsSource;
         private readonly IWorldwideFlightsSource _worldwideFlightsSource;
+        private readonly IDistributedCache<IEnumerable<FlightDto>> _flightCache;
 
-        public FlightApplicationService(ICISFlightsSource cisFlightsSource, IWorldwideFlightsSource worldwideFlightsSource)
+        public FlightApplicationService(ICISFlightsSource cisFlightsSource, IWorldwideFlightsSource worldwideFlightsSource,
+            IDistributedCache<IEnumerable<FlightDto>> flightCache)
         {
             _cisFlightsSource = cisFlightsSource;
             _worldwideFlightsSource = worldwideFlightsSource;
+            _flightCache = flightCache;
         }
 
         public async Task<IEnumerable<FlightDto>> GetFlightsAsync(DateTime departureDate, string deaprtureCity, string destinationCity, double minimumPrice, double maximumPrice, CancellationToken token)
         {
-            var cisFlights = await _cisFlightsSource.GetFlightsAsync(token);
+            var cisFlights = await _flightCache.GetOrAddAsync(
+                "cisFlights",
+                async () => await _cisFlightsSource.GetFlightsAsync(token),
+                () => new DistributedCacheEntryOptions
+                {
+                    AbsoluteExpiration = DateTimeOffset.Now.AddHours(1)
+                }, null, false, token);
+
             var worldwideFlights = await _worldwideFlightsSource.GetFlightsAsync(token);
 
             var filteredCisFlights = GetFilteredList(cisFlights, departureDate, deaprtureCity, destinationCity, minimumPrice, maximumPrice);
@@ -44,8 +56,13 @@ namespace Stupeni.FSA.Flight
 
             var filteredList = flights
                 .Where(x => x.DepartureCity == deaprtureCity && x.DestinationCity == destinationCity)
-                .Where(x => x.DaysOfOperation.Contains(dayOfDeparture))
-                .Where(x => x.Price > (minimumPrice ?? 0) && x.Price < (maximumPrice ?? double.MaxValue));
+                .Where(x => x.DaysOfOperation.Contains(dayOfDeparture));
+
+            if (minimumPrice != null)
+                filteredList.Where(x => x.Price > minimumPrice);
+
+            if (maximumPrice != null)
+                filteredList.Where(x => x.Price < maximumPrice);
 
             return filteredList;
         }
