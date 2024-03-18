3400e7960fe5ce01f6274c4c8916204b3042cbf6
Bekzod Allaev <allaev_@outlook.com>
2024-03-18 18:52:09 +0200

    Add WebApi which returns CIS flights

---
+++ src/Stupeni.FSA.CSIFlightsSourceAPI/Controllers/FlightsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Stupeni.FSA.CSIFlightsSourceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        // GET: api/<FlightsController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using StreamReader r = new StreamReader("flights.json");
+            string json = r.ReadToEnd();
+
+            return Ok(json);
+        }
+    }
+}
